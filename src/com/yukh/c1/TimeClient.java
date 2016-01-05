693f2c0360630fdf6969cb6d3da5e6fa2925c8ca
yukh <yhztykh@163.com>
2016-01-05 23:49:29 +0800

    16/01/05 home finish waio working nio

---
+++ src/com/yukh/c1/TimeClient.java
@@ -0,0 +1,58 @@
+package com.yukh.c1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TimeClient {
+	public static void main(String[] args) {
+		int port = 8889;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				
+			}
+		}
+		Socket socket = null;
+		BufferedReader in = null;
+		PrintWriter out = null;
+		try {
+			socket = new Socket("127.0.0.1", port);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+			out.println("QUERY TIME ORDER");
+			System.out.println("Send order 2 server succeed.");
+			String resp = in.readLine();
+			System.out.println("Now is : " + resp);
+		} catch (Exception e) {
+			
+		} finally {
+			if (out != null) {
+				try {
+					out.close();
+					out = null;
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+			if (in != null) {
+				try {
+					in.close();
+					in = null;
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+			if (socket != null) {
+				try {
+					socket.close();
+					socket = null;
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+}
---
+++ src/com/yukh/c1/TimeServer.java
@@ -0,0 +1,35 @@
+package com.yukh.c1;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+	public static void main(String[] args) throws IOException {
+		int port = 8889;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (Exception e) {
+				
+			}
+		} 
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			System.out.println("The server is start in port : " + port);
+			Socket socket = null;
+			TimeServerHandlerExecutePool singleExecutor = new TimeServerHandlerExecutePool(50, 10000);
+			while (true) {
+				socket = server.accept();
+				singleExecutor.execute(new TimeServerHandler(socket));
+			}
+		} finally {
+			if (server != null) {
+				System.out.println("The time server close");
+				server.close();
+				server = null;
+			}
+		}
+	}
+}
